6fd2bcb7c3ff969a4239f8f0315d360fc00b41af
Muxsin <muxsin.shokirov@gmail.com>
2025-08-09 00:23:02 +0700

    created custom type response for get people

--- projects/people/internal/handlers/http/get.go
+++ projects/people/internal/handlers/http/get.go
@@ -6,7 +6,16 @@ import (
 	"gorm.io/gorm"
 	"log"
 	"net/http"
+	"time"
 )
+
+type getPeopleResponse struct {
+	Id        string    `json:"id"`
+	Name      string    `json:"name"`
+	Surname   string    `json:"surname"`
+	Birthdate time.Time `json:"birthdate"`
+	CreatedAt string    `json:"created_at"`
+}
 
 func (h *handler) Get(c *gin.Context) {
 	id := c.Param("id")
@@ -23,5 +32,13 @@ func (h *handler) Get(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, people)
+	response := getPeopleResponse{
+		Id:        id,
+		Name:      people.Name,
+		Surname:   people.Surname,
+		Birthdate: people.Birthdate,
+		CreatedAt: people.CreatedAt.Format(time.RFC3339),
+	}
+
+	c.JSON(http.StatusOK, response)
 }

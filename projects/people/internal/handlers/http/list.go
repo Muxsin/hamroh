556bbd11557ea17beab8179a94d077ff92185d33
Muxsin <muxsin.shokirov@gmail.com>
2025-08-09 00:29:28 +0700

    created custom type response for listing people

--- projects/people/internal/handlers/http/list.go
+++ projects/people/internal/handlers/http/list.go
@@ -4,7 +4,16 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"time"
 )
+
+type peopleResponse struct {
+	Id        uint      `json:"id"`
+	Name      string    `json:"name"`
+	Surname   string    `json:"surname"`
+	Birthdate time.Time `json:"birthdate"`
+	CreatedAt string    `json:"created_at"`
+}
 
 func (h *handler) List(c *gin.Context) {
 	peoples, err := h.useCase.GetAll()
@@ -15,5 +24,17 @@ func (h *handler) List(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, peoples)
+	var response []peopleResponse
+
+	for _, people := range peoples {
+		response = append(response, peopleResponse{
+			Id:        people.ID,
+			Name:      people.Name,
+			Surname:   people.Surname,
+			Birthdate: people.Birthdate,
+			CreatedAt: people.CreatedAt.Format(time.RFC3339),
+		})
+	}
+
+	c.JSON(http.StatusOK, response)
 }
